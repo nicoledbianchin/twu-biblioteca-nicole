1a6b944b8a953716baf6be457079848338a72de2
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-08 23:30:03 -0300

    show list of books in command line

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,22 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.domain.Book;
+import com.twu.biblioteca.domain.Library;
+
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        Library library = new Library();
+        ArrayList<Book> list = library.getListOfBooks();
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+
+        System.out.println("\nSee our list of books available:");
+        System.out.println("_________________________________");
+        for (Book book : list) {
+            System.out.println(book.getName());
+        }
+
     }
 }
