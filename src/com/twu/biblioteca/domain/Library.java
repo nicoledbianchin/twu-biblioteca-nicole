8539347a38cf96d38ba711ae95256ed9b3cfaff6
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-08 23:23:14 -0300

    add more books

--- src/com/twu/biblioteca/domain/Library.java
+++ src/com/twu/biblioteca/domain/Library.java
@@ -8,6 +8,12 @@ public class Library {
 
     public Library() {
         listOfBooks.add(new Book("Harry Potter and the Philosopher's Stone"));
+        listOfBooks.add(new Book("Harry Potter and the Chamber of Secrets"));
+        listOfBooks.add(new Book("Harry Potter and the Prisioner of Azkaban"));
+        listOfBooks.add(new Book("Harry Potter and the Glober of Fire"));
+        listOfBooks.add(new Book("Harry Potter and the Order of the Phoenix"));
+        listOfBooks.add(new Book("Harry Potter and the Half-Blood Prince"));
+        listOfBooks.add(new Book("Harry Potter and the Deathly Hollows"));
     }
 
     public ArrayList<Book> getListOfBooks() {
--- test/com/twu/biblioteca/bibliotecaService/BibliotecaServiceTest.java
+++ test/com/twu/biblioteca/bibliotecaService/BibliotecaServiceTest.java
@@ -17,6 +17,12 @@ public class BibliotecaServiceTest {
 
     ArrayList<Book> expectedList= new ArrayList();
     expectedList.add(new Book("Harry Potter and the Philosopher's Stone"));
+    list.add(new Book("Harry Potter and the Chamber of Secrets"));
+    list.add(new Book("Harry Potter and the Prisioner of Azkaban"));
+    list.add(new Book("Harry Potter and the Glober of Fire"));
+    list.add(new Book("Harry Potter and the Order of the Phoenix"));
+    list.add(new Book("Harry Potter and the Half-Blood Prince"));
+    list.add(new Book("Harry Potter and the Deathly Hollows"));
 
     for (int j = 0; j < expectedList.size(); j++){
         Assert.assertEquals(expectedList.get(j).getName(), list.get(j).getName());
