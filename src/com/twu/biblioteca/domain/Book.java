140307c60440d31eac9a704afd1f9888a90349f2
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-16 11:21:20 -0300

    add author to the book

--- src/com/twu/biblioteca/domain/Book.java
+++ src/com/twu/biblioteca/domain/Book.java
@@ -2,13 +2,19 @@ package com.twu.biblioteca.domain;
 
 public class Book {
 
+    public String author;
     String name;
 
-    public Book(String name) {
+    public Book(String name, String author) {
         this.name = name;
+        this.author = author;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getAuthor() {
+        return author;
     }
 }
