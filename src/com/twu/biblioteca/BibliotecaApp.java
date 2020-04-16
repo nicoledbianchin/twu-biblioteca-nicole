c19b1a36203ec5d67fc2da16e4cdfdc27ef88166
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-16 15:59:14 -0300

    app starts using menuservice

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,23 +1,34 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.domain.Book;
-import com.twu.biblioteca.domain.Library;
-import com.twu.biblioteca.service.LibraryService;
+import com.twu.biblioteca.service.MenuService;
+import com.twu.biblioteca.service.OutputFormatterService;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        LibraryService libraryService = new LibraryService();
-        ArrayList<Book> list = libraryService.getListOfBooks();
+        MenuService menuService = new MenuService();
+        OutputFormatterService outputFormatterService = new OutputFormatterService();
+        Scanner entry = new Scanner(System.in);
+
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
 
-        System.out.println("\nSee our list of books available:");
-        System.out.println("_________________________________");
-        for (Book book : list) {
-            System.out.println(libraryService.toString(book));
+        System.out.println("1 - List of books");
+        int userOption = entry.nextInt();
+
+
+        switch (userOption) {
+            case 1:
+                System.out.println("\nSee our list of books available:");
+                System.out.println("_________________________________");
+                    for (Book book : menuService.getListOfBooks()) {
+                        System.out.printf(outputFormatterService.formatOutputBooks(book));
+                }
         }
 
+
+        entry.close();
     }
 }
