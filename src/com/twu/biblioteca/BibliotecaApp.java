68070219656a3fe9a21a576c075b73f308306fa8
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-16 17:01:23 -0300

    separates menu from app

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,6 @@
 package com.twu.biblioteca;
 
-import com.twu.biblioteca.domain.Book;
 import com.twu.biblioteca.service.MenuService;
-import com.twu.biblioteca.service.OutputFormatterService;
 
 import java.util.Scanner;
 
@@ -10,24 +8,13 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         MenuService menuService = new MenuService();
-        OutputFormatterService outputFormatterService = new OutputFormatterService();
         Scanner entry = new Scanner(System.in);
 
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-
-        System.out.println("1 - List of books");
+        menuService.showMenu();
         int userOption = entry.nextInt();
 
-
-        switch (userOption) {
-            case 1:
-                System.out.println("\nSee our list of books available:");
-                System.out.println("_________________________________");
-                    for (Book book : menuService.getListOfBooks()) {
-                        System.out.printf(outputFormatterService.formatOutputBooks(book));
-                }
-        }
-
+        menuService.manipulateMenu(userOption);
 
         entry.close();
     }
--- src/com/twu/biblioteca/service/MenuService.java
+++ src/com/twu/biblioteca/service/MenuService.java
@@ -3,9 +3,32 @@ package com.twu.biblioteca.service;
 import com.twu.biblioteca.domain.Book;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuService {
     private LibraryService libraryService = new LibraryService();
+    OutputFormatterService outputFormatterService = new OutputFormatterService();
+    private Map<Integer, String> userOptions = new HashMap<>();
+
+    public MenuService() {
+        userOptions.put(1, "1 - List of books");
+    }
+
+    public void showMenu() {
+        System.out.println(userOptions.get(1));
+    }
+
+    public void manipulateMenu(int userOption) {
+        switch (userOption) {
+            case 1:
+                System.out.println("\nSee our list of books available:");
+                System.out.println("_________________________________");
+                for (Book book : getListOfBooks()) {
+                    System.out.printf(outputFormatterService.formatOutputBooks(book));
+                }
+        }
+    }
 
     public ArrayList<Book> getListOfBooks() {
         return libraryService.getListOfBooks();
