fe9955a408435db9d9859c6788de65fd80d09c0d
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-16 11:34:32 -0300

    refactor app to show author and published year

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,20 +2,23 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.domain.Book;
 import com.twu.biblioteca.domain.Library;
+import com.twu.biblioteca.service.LibraryService;
 
 import java.util.ArrayList;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        Library library = new Library();
-        ArrayList<Book> list = library.getListOfBooks();
+        LibraryService libraryService = new LibraryService();
+        ArrayList<Book> list = libraryService.getListOfBooks();
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
 
         System.out.println("\nSee our list of books available:");
         System.out.println("_________________________________");
         for (Book book : list) {
             System.out.println(book.getName());
+            System.out.println(book.getAuthor());
+            System.out.println(book.getPublishedYear() + "\n");
         }
 
     }
