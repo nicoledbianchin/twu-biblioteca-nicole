3d70df466152ee8bd673fc859ca29624881af17c
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-16 11:31:09 -0300

    ensures date of release is not null

--- test/com/twu/biblioteca/bibliotecaService/LibraryServiceTest.java
+++ test/com/twu/biblioteca/bibliotecaService/LibraryServiceTest.java
@@ -21,13 +21,13 @@ public class LibraryServiceTest {
     @Before
     public void init() {
         expectedList = new ArrayList();
-        expectedList.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling"));
-        list.add(new Book("Harry Potter and the Chamber of Secrets", "J.K. Rowling"));
-        list.add(new Book("Harry Potter and the Prisioner of Azkaban", "J.K. Rowling"));
-        list.add(new Book("Harry Potter and the Globet of Fire", "J.K. Rowling"));
-        list.add(new Book("Harry Potter and the Order of the Phoenix", "J.K. Rowling"));
-        list.add(new Book("Harry Potter and the Half-Blood Prince", "J.K. Rowling"));
-        list.add(new Book("Harry Potter and the Deathly Hollows", "J.K. Rowling"));
+        expectedList.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997));
+        list.add(new Book("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998));
+        list.add(new Book("Harry Potter and the Prisioner of Azkaban", "J.K. Rowling", 1999));
+        list.add(new Book("Harry Potter and the Globet of Fire", "J.K. Rowling", 2000));
+        list.add(new Book("Harry Potter and the Order of the Phoenix", "J.K. Rowling", 2003));
+        list.add(new Book("Harry Potter and the Half-Blood Prince", "J.K. Rowling", 2005));
+        list.add(new Book("Harry Potter and the Deathly Hollows", "J.K. Rowling", 2007));
     }
 
     @Test
@@ -38,9 +38,16 @@ public class LibraryServiceTest {
     }
 
     @Test
-    public void souldHaveAnAuthorPerBook() {
+    public void souldHaveAuthorPerBook() {
         for (int i = 0; i < expectedList.size(); i++) {
             Assert.assertThat(expectedList.get(i).getAuthor(), is(not(equalTo(null))));
+        }
+    }
+
+    @Test
+    public void shouldHavePublishedYearPerBook() {
+        for (int i = 0; i < expectedList.size(); i++) {
+            Assert.assertThat(expectedList.get(i).getPublishedYear(), is(not(equalTo(null))));
         }
     }
 
