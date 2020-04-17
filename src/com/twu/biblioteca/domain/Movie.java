465cdee8ccb5ec9ee5a3ea39086e2e7bb7c61238
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-17 13:38:45 -0300

    construcs movie

---
+++ src/com/twu/biblioteca/domain/Movie.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca.domain;
+
+public class Movie {
+
+    private String name;
+    private String director;
+    private int publishedYear;
+    private int rate;
+
+    public Movie(String name, String director, int publishedYear, int rate) {
+        this.name = name;
+        this.director = director;
+        this.publishedYear = publishedYear;
+        this.rate = rate;
+    }
+
+    public Movie(String name, String director, int publishedYear) {
+        this.name = name;
+        this.director = director;
+        this.publishedYear = publishedYear;
+    }
+}
