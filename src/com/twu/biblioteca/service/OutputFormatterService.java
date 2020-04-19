34dc4d070be457189caf96ea0461edf78cedf0f4
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-19 00:29:55 -0300

    adjust output

--- src/com/twu/biblioteca/service/OutputFormatterService.java
+++ src/com/twu/biblioteca/service/OutputFormatterService.java
@@ -1,10 +1,19 @@
 package com.twu.biblioteca.service;
 
 import com.twu.biblioteca.domain.Book;
+import com.twu.biblioteca.domain.Movie;
 
 public class OutputFormatterService {
 
     public String formatOutputBooks(Book book) {
-        return String.format( "%s, %s, %d\n", book.getName(), book.getAuthor(), book.getPublishedYear());
+        return String.format( "%s, written by %s, published in %d\n", book.getName(), book.getAuthor(), book.getPublishedYear());
+    }
+
+    public String formatOutputMovies(Movie movie){
+        if (movie.getRate() == -1) {
+            return String.format("%s, %s, %d\n", movie.getName(), movie.getDirector(), movie.getPublishedYear());
+        } else {
+            return String.format("%s, directed by %s, released in %d, rating: %d\n", movie.getName(), movie.getDirector(), movie.getPublishedYear(), movie.getRate());
+        }
     }
 }
