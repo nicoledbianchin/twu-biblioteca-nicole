80a3f823aac0c6e2fe05fd522a0e8cc84dd046be
nicoledbianchin <nicoledbianchin@gmail.com>
2020-04-19 00:32:42 -0300

    tests to output

--- test/com/twu/biblioteca/bibliotecaService/OutputFormatterServiceTest.java
+++ test/com/twu/biblioteca/bibliotecaService/OutputFormatterServiceTest.java
@@ -1,4 +1,35 @@
 package com.twu.biblioteca.bibliotecaService;
 
+import com.twu.biblioteca.domain.Book;
+import com.twu.biblioteca.domain.Movie;
+import com.twu.biblioteca.service.OutputFormatterService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
 public class OutputFormatterServiceTest {
+
+    private OutputFormatterService outputFormatterService = new OutputFormatterService();
+
+    @Test
+    public void shouldReturnStringFormattedWithBookInformations() {
+        Book book = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997);
+
+        String actualString = outputFormatterService.formatOutputBooks(book);
+        String expectedString = "Harry Potter and the Philosopher's Stone, written by J.K. Rowling, published in 1997\n";
+
+        Assert.assertThat(actualString, is(equalTo(expectedString)));
+    }
+
+    @Test
+    public void shouldReturnStringFormattedWithRate() {
+        Movie movie = new Movie("The Lord of the Rings: The Fellowship of the Ring", "Peter Jackson", 2002, 9);
+
+        String actualString = outputFormatterService.formatOutputMovies(movie);
+        String expectedString = "The Lord of the Rings: The Fellowship of the Ring, directed by Peter Jackson, released in 2002, rating: 9\n";
+
+        Assert.assertThat(actualString, is(equalTo(expectedString)));
+    }
 }
